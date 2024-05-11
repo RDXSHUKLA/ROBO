e5caf025d534e5b4e0e50c9364e1954a9d0134e9
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-05-11 14:39:28 +0530

    Update karma.py

--- Doraemon/karma.py
+++ Doraemon/karma.py
@@ -8,7 +8,16 @@ from JARVISROBO import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT, BOT_NAME
 
 # <============================================== CONSTANTS =========================================================>
 STATS_IMG = [
-    "https://graph.org/file/8dcccaaa09c3ec38d9c75.jpg", "https://graph.org/file/74d2385efc329c13c11e9.jpg", "https://graph.org/file/d818146f35f6a439a7a7f.jpg", "https://graph.org/file/d1d68eaaa8aecc68f8387.jpg", "https://graph.org/file/257fe1ec8828b836c70f7.jpg", "https://graph.org/file/8b044edab3d3173544439.jpg", "https://graph.org/file/8ed87bfd7c0b3dbdd1bf5.jpg", "https://graph.org/file/1a33887db1a3b5dee1b0a.jpg",  "https://graph.org/file/04ea07e42660988229834.jpg", "https://graph.org/file/abb6b4bb00e2751bc9f54.jpg",
+    "https://graph.org/file/8dcccaaa09c3ec38d9c75.jpg",
+    "https://graph.org/file/74d2385efc329c13c11e9.jpg",
+    "https://graph.org/file/d818146f35f6a439a7a7f.jpg",
+    "https://graph.org/file/d1d68eaaa8aecc68f8387.jpg",
+    "https://graph.org/file/257fe1ec8828b836c70f7.jpg",
+    "https://graph.org/file/8b044edab3d3173544439.jpg",
+    "https://graph.org/file/8ed87bfd7c0b3dbdd1bf5.jpg",
+    "https://graph.org/file/1a33887db1a3b5dee1b0a.jpg",
+    "https://graph.org/file/04ea07e42660988229834.jpg",
+    "https://graph.org/file/abb6b4bb00e2751bc9f54.jpg",
 ]
 
 HEY_IMG = "https://telegra.ph/file/9458a724ae8ebe1de8565.png"
